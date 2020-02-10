6ce5a13c312106461c7243a9ea4d6ad219c64cd5
mare hunt <mare.hunt@netgroup.ee>
2020-02-10 17:55:02 +0200

    test for creating a page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -6,10 +6,12 @@ import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
+import TestPagePreview from './preview-templates/TestPagePreview'
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
+CMS.registerPreviewTemplate('index', TestPagePreview)
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
